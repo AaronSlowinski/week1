e1908a36de1db9106fb8657160d087a16b9da526
Aaron <Aaron.Slowinski@gmail.com>
2024-05-04 12:43:36 -0700

    alter tbl orders

--- week1_workshop/workshop1.sql
+++ week1_workshop/workshop1.sql
@@ -114,6 +114,10 @@ ADD CONSTRAINT fk_orders_customers
 FOREIGN KEY (customer_id)
 REFERENCES customers (id);
 
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_employees
+FOREIGN KEY (employee_id)
+REFERENCES employees (id);
 
 
 
